881ad15052692645ab1f9339a97eacbc320af9a1
ktf <not.discostoo@gmail.com>
2012-08-27 13:53:33 -0700

    test two letters

--- test_letterroll.py
+++ test_letterroll.py
@@ -12,6 +12,12 @@ class TestGenRegExp(unittest.TestCase):
         re = l.get_reg_expressions("a")
         self.assertEqual(re[0], r"[a-z]*a[a-z]*")
 
+    def test_two_letters(self):
+        # sort so we can check for equality more easily
+        re = sorted(l.get_reg_expressions("ab"))
+        regexp = sorted([r"[a-z]*a[a-z]*b[a-z]*", r"[a-z]*b[a-z]*a[a-z]*"])
+        self.assertEqual(re, regexp)
+
 
 if __name__ == "__main__":
     unittest.main()
