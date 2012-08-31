6f4c76724ad1fe39748810932f7a78b4c76204e9
ktf <not.discostoo@gmail.com>
2012-08-30 18:12:39 -0700

    fix borked routing, which fixed 100% CPU bug

--- web_letterroll.py
+++ web_letterroll.py
@@ -9,29 +9,28 @@ import letterroll
 # config
 DEBUG = True
 SECRET_KEY = 'development key'
+DICTIONARY = 'american-english'
 
 
 # create app!
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True) # override!
 
 
 @app.route('/', methods=['POST', 'GET'])
 def show_form():
     if request.method == 'POST':
-        return redirect('/' + request.form['q'])
+        return redirect('/results/' + request.form['q'])
 
-    return render_template('form.html')
+    return render_template('layout.html')
 
 
-@app.route('/<letters>')
+@app.route('/results/<letters>')
 def show_results_for_letters(letters):
     # open dictionary
-    dictionary = open('american-english')
-#    matching_words = letterroll.get_matching_words(letters, dictionary)
-    # TODO: don't pass entire dictionary, huge waste o' memory
-    matching_words = ['fuck', 'you', 'man', 'jeez']
+    dictionary = open(DICTIONARY)
+    # TODO: don't pass entire dictionary, huge waste o' memory?
+    matching_words = letterroll.get_matching_words(letters, dictionary)
     dictionary.close()
     return render_template('show_results.html', letters=letters, matching_words=matching_words)
 
