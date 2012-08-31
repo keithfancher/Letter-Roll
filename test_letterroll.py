5669600c0396e65987dcb99b0814261160ef8de5
ktf <not.discostoo@gmail.com>
2012-08-31 13:24:17 -0700

    test the web interface

--- test_letterroll.py
+++ test_letterroll.py
@@ -4,6 +4,7 @@
 import unittest
 
 import letterroll as l
+import web_letterroll as w
 
 
 class TestGenRegExp(unittest.TestCase):
@@ -44,6 +45,50 @@ class TestWordMatchesRegExpList(unittest.TestCase):
         word = "giantboner"
         regexp_list = [r"asdfsdafsd", r"bnr", r"cray cray"]
         self.assertFalse(l.word_matches_regexp_list(word, regexp_list))
+
+
+class TestWebInterface(unittest.TestCase):
+
+    def setUp(self):
+        w.app.config['TESTING'] = True
+        self.app = w.app.test_client()
+
+    def test_index_exists(self):
+        r = self.app.get('/')
+        assert "Enter the letters you've rolled" in r.data
+        assert r.status_code == 200
+
+    def test_url_request(self):
+        """Passing your letters directly to the URL should work"""
+        r = self.app.get('/results/ffff')
+        assert "riffraff" in r.data
+        assert r.status_code == 200
+
+    def test_post_data(self):
+        """Sending your letters as POST data should work too!"""
+        r = self.app.post('/', data=dict(q='ffff'), follow_redirects=True)
+        assert "riffraff" in r.data
+        assert r.status_code == 200
+
+    def test_empty_string_passed(self):
+        """Passing an empty string should redirect to index"""
+        r = self.app.get('/results/')
+        assert "Enter the letters you've rolled" in r.data
+        assert r.status_code == 200
+
+    def test_dumb_string_returns_no_results(self):
+        r = self.app.get('/results/xxx')
+        assert "No matches" in r.data
+        assert r.status_code == 200
+
+    def test_nonsense_returns_404(self):
+        r = self.app.get('/asdfasdfsdaf')
+        assert r.status_code == 404
+
+    def test_resulsts_case_insensitive(self):
+        r = self.app.get('/results/fFfF')
+        assert "riffraff" in r.data
+        assert r.status_code == 200
 
 
 if __name__ == "__main__":
