65112d90b2012b926f5737441b42a36e836c6663
ktf <not.discostoo@gmail.com>
2012-08-30 17:14:03 -0700

    some styling and small cleanups

--- web_letterroll.py
+++ web_letterroll.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+#!/usr/bin/env python
+
+
+from flask import Flask, request, redirect, url_for, render_template
 
 import letterroll
 
@@ -15,11 +17,22 @@ app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True) # override!
 
 
+@app.route('/', methods=['POST', 'GET'])
+def show_form():
+    if request.method == 'POST':
+        return redirect('/' + request.form['q'])
+
+    return render_template('form.html')
+
+
 @app.route('/<letters>')
 def show_results_for_letters(letters):
     # open dictionary
     dictionary = open('american-english')
-    matching_words = letterroll.get_matching_words(letters, dictionary)
+#    matching_words = letterroll.get_matching_words(letters, dictionary)
+    # TODO: don't pass entire dictionary, huge waste o' memory
+    matching_words = ['fuck', 'you', 'man', 'jeez']
+    dictionary.close()
     return render_template('show_results.html', letters=letters, matching_words=matching_words)
 
 
